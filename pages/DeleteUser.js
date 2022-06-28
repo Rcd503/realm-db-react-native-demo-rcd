5930442dda140d7fa14d627474ee6e72da4c7484
Sambad Ramesh <37452999+Rcd503@users.noreply.github.com>
2022-06-28 15:02:48 +0530

    comment code remove

--- pages/DeleteUser.js
+++ pages/DeleteUser.js
@@ -21,6 +21,8 @@ const TaskSchema = {
   },
   primaryKey: 'user_id',
 };
+
+
 const DeleteUser = ({navigation}) => {
   let [inputUserId, setInputUserId] = useState('');
 
@@ -57,33 +59,7 @@ const DeleteUser = ({navigation}) => {
       });
     });
   };
-  // let deleteUser = () => {
-  //   db.transaction((tx) => {
-  //     tx.executeSql(
-  //       'DELETE FROM  table_user where user_id=?',
-  //       [inputUserId],
-  //       (tx, results) => {
-  //         console.log('Results', results.rowsAffected);
-  //         if (results.rowsAffected > 0) {
-  //           Alert.alert(
-  //             'Success',
-  //             'User deleted successfully',
-  //             [
-  //               {
-  //                 text: 'Ok',
-  //                 onPress: () => navigation.navigate('HomeScreen'),
-  //               },
-  //             ],
-  //             { cancelable: false }
-  //           );
-  //         } else {
-  //           alert('Please insert a valid User Id');
-  //         }
-  //       }
-  //     );
-  //   });
-  // };
-
+  
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, backgroundColor: 'white'}}>
